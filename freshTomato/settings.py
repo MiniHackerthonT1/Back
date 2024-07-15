f28eadde2d72eb0f5d0400fe974a2970c03fa82a
parkeunjin020404 <eunjin020404@naver.com>
2024-07-15 10:12:47 +0900

    reqwuirements, User app

--- freshTomato/settings.py
+++ freshTomato/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-l!^ak!v)d5kg@r(hx**e_4$yp=a5^_dxp#51(#p7yjy&ns#-mz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
 
 
