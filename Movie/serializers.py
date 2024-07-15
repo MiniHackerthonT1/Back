104b7a3a4bca4dfce8b341bc18539efbc7d6ed8f
kimdevv <kim__dev@hufs.ac.kr>
2024-07-15 13:41:21 +0900

    [feat] 영화, 배우 데이터 저장 API

---
+++ Movie/serializers.py
@@ -0,0 +1,26 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from allauth.account.adapter import get_adapter
+from rest_framework import serializers
+from .models import Movie, Actor
+
+class SaveActorToDBSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['name', 'character', 'image_url']
+
+class SaveMovieToDBSerializer(serializers.ModelSerializer):
+    actors = SaveActorToDBSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = ['actors', 'title_kor', 'title_eng', 'poster_url', 'genre', 'showtime', 'release_date', 'plot', 'rating', 'director_name', 'director_image_url']
+
+    def create(self, validated_data):
+        actorsData = validated_data.pop('actors')
+        movie = Movie.objects.create(**validated_data)
+
+        for actorData in actorsData:
+            actor, created = Actor.objects.get_or_create(**actorData)
+            movie.actors.add(actor)
+
+        return "완료!"
